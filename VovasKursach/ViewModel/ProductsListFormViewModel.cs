7fb4ef479dca523ce0240e4c003b3e18bff02617
vano hacker <v.gritskevich25@gmail.com>
2020-01-02 23:40:47 +0300

    first commit

---
+++ VovasKursach/ViewModel/ProductsListFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace VovasKursach.ViewModel
+{
+    public class ProductsListFormViewModel : ViewModelBase
+    {
+        public ObservableCollection<Product> Products
+        {
+            get
+            {
+                using (var context = new DBContext())
+                {
+                    var query = from p in context.Products select p;
+
+                    return new ObservableCollection<Product>(query);
+                }
+            }
+        }
+    }
+}
---
+++ VovasKursach/ViewModel/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace VovasKursach.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected ViewModelBase()
+        { }
+
+        public virtual void OnProperyChanged(string propName)
+        {
+            PropertyChangedEventHandler propertyChangedEventHandler = this.PropertyChanged;
+            if (propertyChangedEventHandler != null)
+            {
+                propertyChangedEventHandler.Invoke(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
