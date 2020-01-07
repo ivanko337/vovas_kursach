5140697f6fca6a0fab3023f9b1095d2b3db2ed6d
vano hacker <v.gritskevich25@gmail.com>
2020-01-07 10:25:11 +0300

    pip

--- VovasKursach/ViewModel/AddProductFormViewModel.cs
+++ VovasKursach/ViewModel/AddProductFormViewModel.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Input;
 using VovasKursach.Infrastructure.Commands;
+using VovasKursach.View;
 
 namespace VovasKursach.ViewModel
 {
     public class AddProductFormViewModel : ViewModelBase
     {
-        public Product Product { get; set; } = new Product();
+        public Product Product { get; set; }
 
-        public List<ProductType> ProductTypes
+        public ObservableCollection<ProductType> ProductTypes
         {
             get
             {
@@ -20,8 +21,16 @@ namespace VovasKursach.ViewModel
                 {
                     var query = context.ProductsTypes;
 
-                    return query.ToList();
+                    return new ObservableCollection<ProductType>(query);
                 }
+            }
+        }
+
+        public ObservableCollection<IngridientProduct> IngridientsProducts
+        {
+            get
+            {
+                return new ObservableCollection<IngridientProduct>(Product.IngredientsProducts);
             }
         }
 
@@ -31,9 +40,92 @@ namespace VovasKursach.ViewModel
             {
                 return new Command((obj) =>
                 {
-                    System.Windows.MessageBox.Show(Product.ProductType.TypeName);
+                    using (var context = new KursachDBContext())
+                    {
+                        context.Products.Add(this.Product);
+
+                        try
+                        {
+                            context.SaveChanges();
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Windows.MessageBox.Show(ex.Message);
+                        }
+                    }
                 });
             }
+        }
+
+        public ICommand SelectProductPicture
+        {
+            get
+            {
+                return new Command((obj) =>
+                {
+                    using (var dialog = new OpenFileDialog())
+                    {
+                        dialog.Filter = "jpg files (*.jpg)|*.jpg|png files (*.png)|*.png|bmp files (*.bmp)|*.bmp";
+                        dialog.RestoreDirectory = true;
+
+                        if (dialog.ShowDialog() == DialogResult.OK)
+                        {
+                            Product.ProductImagePath = dialog.FileName;
+
+                            OnProperyChanged(nameof(Product));
+                        }
+                    }
+                });
+            }
+        }
+
+        public ICommand AddIngredient
+        {
+            get
+            {
+                return new Command((obj) =>
+                {
+                    AddIngredientForm form = new AddIngredientForm();
+                    ((AddIngredientViewModel)form.DataContext).NewProduct = this.Product;
+                    form.ShowDialog();
+
+                    OnProperyChanged(nameof(IngridientsProducts));
+                });
+            }
+        }
+
+        public ICommand DeleteIngredients
+        {
+            get
+            {
+                return new Command((obj) =>
+                {
+                    System.Windows.Controls.ListView listView = obj as System.Windows.Controls.ListView;
+
+                    foreach (var item in listView.SelectedItems)
+                    {
+                        Product.IngredientsProducts.Remove(item as IngridientProduct);
+                    }
+
+                    OnProperyChanged(nameof(Product));
+                });
+            }
+        }
+
+        public ICommand CreateProductType
+        {
+            get
+            {
+                return new Command((obj) =>
+                {
+                    // надо допилякать форму
+                });
+            }
+        }
+
+        public AddProductFormViewModel()
+        {
+            Product = new Product();
         }
     }
 }
---
+++ VovasKursach/ViewModel/CreateProductTypeFormViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using VovasKursach.Infrastructure.Commands;
+
+namespace VovasKursach.ViewModel
+{
+    public class CreateProductTypeFormViewModel : ViewModelBase
+    {
+        public ProductType NewType { get; set; }
+
+        public ICommand CreateTypeCommand
+        {
+            get
+            {
+                return new Command(CreateType, CanCreateType);
+            }
+        }
+
+        public CreateProductTypeFormViewModel()
+        {
+            NewType = new ProductType();
+        }
+
+        private void CreateType(object parameter)
+        {
+            using (var context = new KursachDBContext())
+            {
+                context.ProductsTypes.Add(NewType);
+
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    ((Window)parameter).Close();
+                }
+            }
+        }
+
+        private bool CanCreateType(object parameter)
+        {
+            return NewType != null && !string.IsNullOrEmpty(NewType.TypeName);
+        }
+    }
+}
