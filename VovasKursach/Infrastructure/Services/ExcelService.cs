04393863b30d6a5708cbf5042bdee41d12fb6d80
vano hacker <v.gritskevich25@gmail.com>
2020-01-11 09:40:18 +0300

    add reports

---
+++ VovasKursach/Infrastructure/Services/ExcelService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace VovasKursach.Infrastructure.Services
+{
+    public class ExcelService
+    {
+        private int MaxLenValue(IEnumerable<Product> products, Func<Product, string> func)
+        {
+
+
+            return 0;
+        }
+
+        public void WriteToExcel(string path, IEnumerable<Product> products)
+        {
+            if (string.IsNullOrEmpty(path) && File.Exists(path))
+            {
+                System.Windows.MessageBox.Show("Неверный путь");
+                return;
+            }
+
+            var excel = new Application();
+
+            excel.Visible = false;
+            excel.DisplayAlerts = false;
+
+            _Workbook workBook = excel.Workbooks.Open(path);
+            _Worksheet workSheet = (_Worksheet)workBook.ActiveSheet;
+            workSheet.Name = "Products";
+
+            workSheet.Cells.ClearContents();
+
+            workSheet.Cells[1, 1] = "Id";
+            workSheet.Cells[1, 1] = "Name";
+            workSheet.Cells[1, 1] = "Product type";
+            workSheet.Cells[1, 1] = "Product image path";
+            workSheet.Cells[1, 1] = "Recipe text";
+            workSheet.Cells[1, 1] = "Price";
+
+
+
+            int index = 2;
+            foreach (Product product in products)
+            {
+                workSheet.Cells[index, 1] = product.Id.ToString();
+                workSheet.Cells[index, 2] = product.Name;
+                workSheet.Cells[index, 3] = product.ProductType.TypeName;
+                workSheet.Cells[index, 4] = product.ProductImagePath;
+                workSheet.Cells[index, 5] = product.RecipeText;
+                workSheet.Cells[index, 6] = product.Price.ToString();
+
+                ++index;
+            }
+
+            workBook.Save();
+
+            workBook?.Close();
+        }
+    }
+}
--- VovasKursach/ViewModel/ProductsListFormViewModel.cs
+++ VovasKursach/ViewModel/ProductsListFormViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Windows.Input;
 using VovasKursach.Infrastructure.Commands;
 using VovasKursach.View;
+using VovasKursach.Infrastructure.Services;
 
 namespace VovasKursach.ViewModel
 {
@@ -46,6 +47,14 @@ namespace VovasKursach.ViewModel
             }
         }
 
+        public ICommand CreateReportCommand
+        {
+            get
+            {
+                return new Command(CreateReport);
+            }
+        }
+
         private void ProductDetails(object parameter)
         {
             Product p = parameter as Product;
@@ -78,6 +87,12 @@ namespace VovasKursach.ViewModel
         {
             Product product = parameter as Product;
 
+            if (product == null)
+            {
+                System.Windows.MessageBox.Show("Выберите элемент для удаления!", "WARNING", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Warning);
+                return;
+            }
+
             using (var context = new KursachDBContext())
             {
                 var deleteEntity = context.Products.FirstOrDefault((p) => p.Id == product.Id);
@@ -88,6 +103,51 @@ namespace VovasKursach.ViewModel
                     context.SaveChanges();
 
                     OnProperyChanged(nameof(Products));
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void CreateReport(object parameter)
+        {
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            dialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+            dialog.RestoreDirectory = true;
+            string path = "";
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = dialog.FileName;
+            }
+            else
+            {
+                return;
+            }
+
+            using (var context = new KursachDBContext())
+            {
+                var service = new ExcelService();
+                var query = from p in context.Products
+                            select p;
+                /*
+                 new Product()
+                            {
+                                Id = p.Id,
+                                Name = p.Name,
+                                ProductImagePath = p.ProductImagePath,
+                                ProductType = p.ProductType,
+                                RecipeText = p.RecipeText
+                            };
+                 */
+
+                try
+                {
+                    service.WriteToExcel(path, query);
+
+                    System.Windows.MessageBox.Show("All done!");
                 }
                 catch (Exception ex)
                 {
